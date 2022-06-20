f0d3f11b9550a26cc16577eba7f9e10f46f4100c
Andrew Tran <andrew_tran@ucsb.edu>
2022-06-19 20:45:19 -0700

    python!

---
+++ screen-cal.py
@@ -0,0 +1,19 @@
+import subprocess
+
+def getDate():
+    pass
+
+def getMonitor():
+    pass
+
+def extractMonitorOpts():
+    pass
+
+def genCommand():
+    pass
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
